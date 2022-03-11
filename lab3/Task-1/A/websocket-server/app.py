7a94575f9b5c953e7b8ba744c563eef720f04507
anastasiia <persikk3000@gmail.com>
2022-03-11 12:49:10 +0300

    add 1A task

---
+++ lab3/Task-1/A/websocket-server/app.py
@@ -0,0 +1,35 @@
+import socket
+
+
+def start_my_server():
+    try:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(('127.0.0.1', 2000))
+        server.listen(4)
+        print('Working...')
+        while True:
+            client_socket, address = server.accept()
+            data = client_socket.recv(1024).decode('utf-8')
+            content = load_from_request(data)
+            client_socket.send(content)
+            client_socket.shutdown(socket.SHUT_WR)
+    except KeyboardInterrupt:
+        server.close()
+
+
+def load_from_request(request_data):
+    HDRS_200 = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
+    HDRS_404 = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
+    path = request_data.split(' ')[1]
+    print(path)
+    response = ''
+    try:
+        with open('views' + path, 'rb') as file:
+            response = file.read()
+        return HDRS_200.encode('utf-8') + response
+    except FileNotFoundError:
+        return (HDRS_404 + 'Page not found').encode('utf-8')
+
+
+if __name__ == '__main__':
+    start_my_server()
