99c71e0b1acf0787dcff5eba50593fc94f37b274
anastasiia <persikk3000@gmail.com>
2022-04-02 21:18:07 +0300

    add filezilla task

---
+++ lab6/ftp_server_filezilla/ftp_server.py
@@ -0,0 +1,23 @@
+import os
+
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+def run_server():
+    authorizer = DummyAuthorizer()
+    authorizer.add_user('test_user', 'test123!', '.', perm='elradfmwMT')
+
+    handler = FTPHandler
+    handler.authorizer = authorizer
+    handler.banner = "pyftpdlib based ftpd ready."
+
+    address = ('127.0.0.1', 23)
+    server = FTPServer(address, handler)
+    
+    server.max_cons = 256
+    server.max_cons_per_ip = 5
+
+    server.serve_forever()
+    
+run_server()
