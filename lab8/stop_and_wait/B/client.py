8dd4f06adad94ea6650c3e23bdaea78495e7c1b5
Anaiya798 <persikk3000@gmail.com>
2022-04-16 17:42:07 +0300

    add 1B task

---
+++ lab8/stop_and_wait/B/client.py
@@ -0,0 +1,104 @@
+import socket
+from random import uniform
+
+
+def read_file(filename, chunk_size):
+    try:
+        file = open(filename, "rb")
+        pkgs = []
+        try:
+            bytes_read = file.read(chunk_size)
+            pkgs.append(bytes_read)
+            while bytes_read:
+                bytes_read = file.read(chunk_size)
+                pkgs.append(bytes_read)
+        except Exception:
+            print(f'Unknown exception while reading from {filename}')
+        finally:
+            file.close()
+        pkgs.append('END'.encode('utf-8'))
+        return pkgs
+    except FileNotFoundError:
+        print(f'No such file or directory {filename}')
+
+
+def receive_file(client, content_size):
+    for i in range(content_size):
+        try:
+            data = client.recvfrom(1024)[0]
+            data = data.decode('utf-8')
+            print(data)
+            try:
+                with open('server_statistics.txt', 'a') as f:
+                    f.write(data)
+            except Exception:
+                print('Unknown error while writing to file: server_statistics.txt')
+        except Exception:
+            print('Request timed out. Package was lost on server!')
+
+
+def client(pkgs, timeout):
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client.connect(('127.0.0.1', 2000))
+    client.settimeout(timeout)
+
+    for i in range(len(pkgs)):
+        loss_threshold = uniform(0, 1)
+        if loss_threshold > 0.7:
+            print(f'Package {i} was lost on client')
+            print()
+            continue
+        else:
+            print('====')
+            print(f'Sending package {i}')
+            header_number = 'PKG_0\r\n'
+            msg = header_number.encode('utf-8') + pkgs[i]
+            client.sendto(msg, ('127.0.0.1', 2000))
+            try:
+                data, addr = client.recvfrom(1024)
+                data = data.decode('utf-8')
+
+                if data[:5] != 'PKG_0' and data[:5] != 'PKG_1' and 'CONTENT SIZE' not in data:
+                    raise Exception
+
+                print(data)
+                print('====')
+                print()
+
+                content_size = int(data[len(data) - 1])
+                if content_size > 0:
+                    receive_file(client, content_size)
+
+            except Exception:
+                print('Request timed out. Sending one more time...')
+                header_number = 'PKG_1\r\n'
+                msg = header_number.encode('utf-8') + pkgs[i]
+                client.sendto(msg, ('127.0.0.1', 2000))
+                try:
+                    data, addr = client.recvfrom(1024)
+                    data = data.decode('utf-8')
+
+                    if data[:5] != 'PKG_0' and data[:5] != 'PKG_1' and 'CONTENT SIZE' not in data:
+                        raise Exception
+
+                    print(data)
+                    print('====')
+                    print()
+
+                    content_size = int(data[len(data) - 1])
+                    if content_size > 0:
+                        receive_file(client, content_size)
+
+                except Exception:
+                    print('Request timed out. Package was lost on server!')
+                    print('====')
+                    print()
+
+
+if __name__ == '__main__':
+    filename = input('File to send: ')
+    chunk_size = int(input('Number of bytes in one package: '))
+    timeout = int(input('Max time for waiting answer from server (seconds): '))
+    pkgs = read_file(filename, chunk_size)
+    pkgs.append('statistics'.encode('utf-8'))
+    client(pkgs, timeout)
---
+++ lab8/stop_and_wait/B/server.py
@@ -0,0 +1,100 @@
+import socket
+from random import uniform
+from datetime import datetime
+
+ACK = 'The server received your package'
+CHUNK_SIZE = 100
+
+
+def read_file(filename):
+    try:
+        file = open(filename, "rb")
+        pkgs = []
+        try:
+            bytes_read = file.read(CHUNK_SIZE)
+            pkgs.append(bytes_read)
+            while bytes_read:
+                bytes_read = file.read(CHUNK_SIZE)
+                pkgs.append(bytes_read)
+        except Exception:
+            print(f'Unknown exception while reading from {filename}')
+        finally:
+            file.close()
+        return pkgs
+    except FileNotFoundError:
+        print(f'No such file or directory {filename}')
+
+
+def send_file(server, addr, pkgs):
+    for i in range(len(pkgs)):
+        loss_threshold = uniform(0, 1)
+        if loss_threshold > 0.7:
+            continue
+        try:
+            server.sendto(pkgs[i], addr)
+        except Exception:
+            print('Connection lost')
+
+
+def add_statistics(data):
+    try:
+        with open('statistics.txt', 'a') as f:
+            f.write('====\n')
+            f.write(f'{datetime.now()}\n')
+            f.write(f'Received {len(data.encode("utf-8"))} bytes\n')
+            f.write(f'Decoded data: {data}\n')
+            f.write('====\n')
+            f.write('\n\n')
+    except Exception:
+        print('Unknown error while writing to file: statistics.txt')
+
+
+def server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server.bind(('127.0.0.1', 2000))
+    print('Server running...')
+    try:
+        while True:
+            content_size = 0
+            pkgs = []
+            acc_files_counter = 1
+
+            data, addr = server.recvfrom(1024)
+            if not data:
+                continue
+            data = data.decode('utf-8')
+            loss_threshold = uniform(0, 1)
+            if loss_threshold > 0.7:
+                continue
+            elif data[:5] != 'PKG_0' and data[:5] != 'PKG_1':
+                continue
+            else:
+                if data[7:] == 'statistics':
+                    pkgs = read_file('statistics.txt')
+                    content_size = len(pkgs)
+
+                ack_msg = data[:5] + ' ' + ACK + '\nCONTENT SIZE: ' + str(content_size)
+                server.sendto(ack_msg.encode('utf-8'), addr)
+
+                if content_size > 0:
+                    send_file(server, addr, pkgs)
+
+                add_statistics(data)
+
+                if data[7:] == 'END':
+                    acc_files_counter += 1
+                    continue
+
+                filename = 'decoded_file_' + str(acc_files_counter) + '.txt'
+                try:
+                    with open(filename, 'a') as f:
+                        f.write(data[7:])
+                except Exception:
+                    print(f'Unknown error while writing to file: {filename}')
+
+    except KeyboardInterrupt:
+        server.close()
+
+
+if __name__ == '__main__':
+    server()
