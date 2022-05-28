1b738a4573ef32ac1fe6a3dbb0544af03c71d5d9
Anaiya798 <persikk3000@gmail.com>
2022-05-28 20:33:48 +0300

    add task 1B

---
+++ lab2/Task-1/simple_service_without_icons/shop_products.py
@@ -0,0 +1,10 @@
+products = [
+    {'id': '0',
+     'name': 'iPhone 11',
+     'category': 'base',
+     'description': 'Apple iPhone 11, 64 GB, black'},
+    {'id': '1',
+     'name': 'iPhone 13',
+     'category': 'pro',
+     'description': 'Apple iPhone 13, 256GB, black'}
+]
